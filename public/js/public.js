efd89fe47a712ca108de6e9706069c3dfa7d811f
Nevma <info@nevma.gr>
2017-01-31 19:05:16 +0200

    Organizing front-end functionality to a JavaScript object

--- public/js/public.js
+++ public/js/public.js
@@ -1,68 +1,87 @@
-jQuery( function ( $ ) {
+var WCATCN = {};
 
-	// The deactivation timeout handler
-	var deactivationTimeoutHandler;
+/**
+ * The default settings
+ * @property {Object} defaults The default settings values.
+ */
+WCATCN.defaults = {
 
-	var defaults = {
-		'deactivationTimeout'         : 5000,
-		'deactivationTimeoutExtended' : 1000,
-		'activationEvent'             : 'added_to_cart',
-		'element'                     : '.wcatcn-wrapper',
-		'dismiss'                     : '.wcatcn-dismiss',
-	}
+	'deactivationTimeout'         : 5000,
+	'deactivationTimeoutExtended' : 1000,
+	'activationEvent'             : 'added_to_cart',
+	'element'                     : '.wcatcn-wrapper',
+	'dismiss'                     : '.wcatcn-dismiss',
 
-	var options = defaults;
+}
 
-	var $notification = $( options.element );
+WCATCN.init = function() {
 
-	if ( $notification.length < 1 ) {
+	this.options = this.defaults;
+
+	// Cache elements
+	this.$body = jQuery( document.body );
+	this.$notification = jQuery( this.options.element );
+	this.$dismiss = jQuery( this.options.dismiss );
+
+	// Bail if notification element doesn't exist
+	if ( ! this.$notification.length ) {
 
 		return;
 
 	}
 
-	/**
-	 * Activates (shows) the notification, and renews the deactivation
-	 * timeout.
+	/*
+	 * Register the event handlers
 	 */
-	function wcatcnActivate() {
-
-		// Prevent deactivation, if scheduled
-		clearTimeout( deactivationTimeoutHandler );
-
-		// Enable the notification
-		$notification.addClass( 'active' );
-
-		// Set a deactivation timeout anew.
-		deactivationTimeoutHandler = setTimeout( wcatcnDeactivate, options.deactivationTimeout );
-
-	}
-
-	/**
-	 * Deactivates (hides) the notification, and clears the deactivation
-	 * timeout.
-	 *
-	 * Clearing the deactivation timeout is necessary since the notification
-	 * can be dismissed on-demand by the user, thus leaving a stray
-	 * deactivation scheduled.
-	 */
-	function wcatcnDeactivate() {
-
-		$notification.removeClass( 'active' );
-
-		clearTimeout( deactivationTimeoutHandler );
-	}
 
 	// Activate the notification on the added_to_cart event
-	$( document.body ).on( options.activationEvent, wcatcnActivate );
+	this.$body.on( this.options.activationEvent, this.activate.bind( this ) );
 
-	// Dismiss the notification when the "Hide" option is clicked
-	$( options.dismiss ).click( function(e) {
+	if ( this.$dismiss.length > 0 ) {
 
-		wcatcnDeactivate();
-		
-		return false;
+		// Dismiss the notification when the "Hide" option is clicked
+		this.$dismiss.on( 'click', this.deactivate.bind( this ) );
 
-	} );
+	}
+
+}
+
+/**
+ * Activates (shows) the notification, and renews the deactivation
+ * timeout.
+ */
+WCATCN.activate = function() {
+
+	// Prevent deactivation, if scheduled
+	clearTimeout( this.timeoutHandler );
+	
+	// Enable the notification
+	this.$notification.addClass( 'active' );
+
+	// Set a deactivation timeout anew.
+	this.timeoutHandler = setTimeout( this.deactivate, this.options.deactivationTimeout );
+}
+
+/**
+ * Deactivates (hides) the notification, and clears the deactivation
+ * timeout.
+ *
+ * Clearing the deactivation timeout is necessary since the notification
+ * can be dismissed on-demand by the user, thus leaving a stray
+ * deactivation scheduled.
+ */
+WCATCN.deactivate = function() {
+	
+	this.$notification.removeClass( 'active' );
+
+	clearTimeout( this.timeoutHandler );
+
+	return false;
+}
+
+
+jQuery( function ( $ ) {
+
+	WCATCN.init();
 
 });
